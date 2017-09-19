5ec903fdd0e075d3539842a190d7ac54106cd0ba
lucaskatt <lkatt@umich.edu>
2017-09-18 22:12:01 -0500

    orbit and basic thrusters

---
+++ Satellite Game/Assets/Scripts/Matter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matter : MonoBehaviour {
+
+    public Planet planet;
+    public float gravConst = 1;
+
+    protected Rigidbody2D body;
+
+	// Use this for initialization
+	virtual protected void Start () {
+        body = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    virtual protected void FixedUpdate()
+    {
+        Vector2 heading = (Vector2) planet.transform.position - body.position;
+        heading.Normalize();
+        body.AddForce(heading * CalculateGravitationalForce()); 
+    }
+
+    private float CalculateGravitationalForce()
+    {
+        float dist = Vector2.Distance(transform.position, planet.transform.position);
+        return gravConst * body.mass * planet.mass / (dist);
+    }
+}
---
+++ Satellite Game/Assets/Scripts/Satellite.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : Matter {
+
+    public float initialVelocity;
+    public float thrusterForce;
+
+    private float escapeVelocity;
+
+	// Use this for initialization
+	override protected void Start () {
+        base.Start();
+        body.velocity = Vector2.right * initialVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.up = -1 * (planet.transform.position - transform.position);
+	}
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            Thrust(Direction.right);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            Thrust(Direction.left);
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            Thrust(Direction.up);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            Thrust(Direction.down);
+        }
+
+        escapeVelocity = CalculateEscapeVelocity();
+        
+    }
+
+    public void Thrust(Direction dir)
+    {
+        Vector2 heading;
+
+        switch (dir)
+        {
+            case Direction.left:
+                heading = -1 * transform.right;
+                break;
+            case Direction.right:
+                heading = transform.right;
+                break;
+            case Direction.up:
+                heading = transform.up;
+                break;
+            case Direction.down:
+                heading = -1 * transform.up;
+                break;
+            default:
+                heading = Vector2.zero;
+                break;
+        }
+
+        body.AddForce(heading * thrusterForce);
+
+    }
+
+    //need to set max velocity
+    private float CalculateEscapeVelocity()
+    {
+        return 0;
+    }
+
+    public enum Direction
+    {
+        left,
+        right,
+        up,
+        down
+    }
+
+}
